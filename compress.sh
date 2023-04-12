fb251c97bdffbc8190c72b3b0755e12f3ee471ba
VitoFe <vito-7@hotmail.it>
2023-04-12 18:31:43 +0200

    Create compress.sh

---
+++ compress.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+display_help() {
+    echo "Usage: $0 [options] input_folder output_folder"
+    echo
+    echo "Compress media files in a given input folder and copy them to a given output folder."
+    echo
+    echo "Options:"
+    echo "  -h, --help      Show this help message and exit"
+    echo "  -q, --quality   Set the quality value for compression (default: 75)"
+    echo
+}
+
+# Default options
+quality=75
+max_processes=4
+
+# Parse the command-line options using getopts
+while getopts ":hq:-:" opt; do
+    case "$opt" in
+        h) # Display help message and exit
+            display_help
+            exit 0
+            ;;
+        q) # Set the quality 0-100
+            quality="$OPTARG"
+            ;;
+        p) # Set the max number of processes to spawn
+            max_processes="$OPTARG"
+            ;;
+        -) # Handle long options
+            case "$OPTARG" in
+                help)
+                    display_help
+                    exit 0
+                    ;;
+                quality=*)
+                    quality="${OPTARG#*=}"
+                    ;;
+                max_processes=*)
+                    max_processes="${OPTARG#*=}"
+                    ;;
+                *) # Invalid option
+                    echo "Error: Invalid option: --$OPTARG" >&2
+                    display_help >&2
+                    exit 1
+                    ;;
+            esac
+            ;;
+        \?) # Invalid option
+            echo "Error: Invalid option: -$OPTARG" >&2
+            display_help >&2
+            exit 1
+            ;;
+        :) # Missing argument
+            echo "Error: Missing argument for -$OPTARG" >&2
+            display_help >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+if [ $# -ne 2 ]; then
+    echo "Error: Missing input_folder or output_folder" >&2
+    display_help >&2
+    exit 1
+fi
+
+input_folder="$1"
+output_folder="$2"
+
+# Create output folder if it does not exist
+mkdir -p "$output_folder"
+
+start_time=$(date +%s)
+
+function convert_files() {
+    for item in "$1"/*; do
+        if [[ -d "$item" ]]; then
+            mkdir -p "$output_folder/${item#$input_folder/}"
+            convert_files "$item"
+        elif [[ -f "$item" ]]; then
+            case "${item##*.}" in
+                jpg|jpeg|png|gif)
+                    printf "Converting %s\n" "$item"
+                    convert -quality "$quality" "$item" "$output_folder/${item#$input_folder/}" &
+                    ;;
+                mp4|avi|mov|mkv)
+                    printf "Compressing %s\n" "$item"
+                    crf=$((51 - quality / 2))
+                    ffmpeg -i "$item" -vcodec libx264 -crf "$crf" "$output_folder/${item#$input_folder/}" &
+                    ;;
+            esac
+
+            # Limit number of processes
+            while (( $(jobs -r -p | wc -l) >= $max_processes )); do
+                sleep 0.1
+            done
+        fi
+    done
+}
+
+convert_files "$input_folder"
+
+wait
+
+size_before=$(find "$input_folder" -type f -exec du -b {} + | awk '{total += $1} END {print total}')
+size_after=$(find "$output_folder" -type f -exec du -b {} + | awk '{total += $1} END {print total}')
+space_saved=$((size_before - size_after))
+saved=$(numfmt --to=iec-i --suffix=B $space_saved)
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+
+echo "Done. Compressed files are copied to $output_folder in $elapsed_time seconds. Saved $saved"
